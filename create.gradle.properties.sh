3a9d85ae1594e7d52c2fb4d67985974ba74f3543
developer <developer@local.com>
2016-03-23 00:53:02 +0200

    little fix

---
+++ create.gradle.properties.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+gradleConfigFile="$HOME/.gradle/gradle.properties"
+
+touch ${gradleConfigFile}
+echo "org.gradle.daemon=true" >> ${gradleConfigFile}
+echo "org.gradle.parallel=true" >> ${gradleConfigFile}
+echo "org.gradle.configureondemand=true" >> ${gradleConfigFile}
