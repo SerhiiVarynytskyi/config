4879bfde0cd85f31be140b6e558a65fde8788c76
developer <developer@local.com>
2016-03-24 09:45:53 +0200

    little fix

--- create.gradle.properties.sh
+++ create.gradle.properties.sh
@@ -5,5 +5,5 @@ gradleConfigFile="$HOME/.gradle/gradle.properties"
 
 touch ${gradleConfigFile}
 echo "org.gradle.daemon=true" >> ${gradleConfigFile}
-echo "org.gradle.parallel=true" >> ${gradleConfigFile}
+#echo "org.gradle.parallel=true" >> ${gradleConfigFile}
 echo "org.gradle.configureondemand=true" >> ${gradleConfigFile}
